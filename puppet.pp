bf8556757d45f152932db9490c94bf733470eced
Денис Туляков <dtulyakov@gmail.com>
2016-12-04 18:27:15 +0300

    На ветке task2 	изменено:      puppet.pp

--- puppet.pp
+++ puppet.pp
@@ -1,14 +1,4 @@
-#absent_file_exec
-
-#  exec { 'create_needed_directory':
-#    command => '/bin/mkdir -p /tmp/needed/directory',
-#    creates => '/tmp/needed/directory'
-#  }
-
-
-
 # Установка и запуск tomcat6
-#class tomcat6 {
   package { 'tomcat6':
     ensure => 'installed',
   }
@@ -16,10 +6,8 @@
     enable => true,
     ensure => running,
   }
-#}
 
 # Установка и запуск Nginx
-#class nginx {
   package { 'nginx':
     ensure => 'installed',
   }
@@ -27,5 +15,4 @@
     enable => true,
     ensure => running,
   }
-#}
 
